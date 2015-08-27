e31e3a413ac1d6daa313855beea3de5fea60d115
CAGD Student <cagd495@mailinator.com>
2015-08-27 15:49:17 -0700

    no message

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,54 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    /*
+	public float speed = 6.0f;
+	public float jumpSpeed = 9.0f;
+	public float gravity = 15.0f;
+	public bool platform = false; 
+	public GameObject movingPlatform;
+	public bool activePlatform = false;
+
+	private Vector3 movement = Vector3.zero;
+	private MovingPlatform movingplatform;
+	
+	void Update () { 
+
+		CharacterController controller = GetComponent<CharacterController> ();
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (new Vector3 (Time.deltaTime * speed, 0, 0));
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (new Vector3 (-Time.deltaTime * speed, 0, 0));
+		}
+		if (controller.isGrounded) {
+			if (Input.GetKey (KeyCode.Space)) {
+				movement.y = jumpSpeed;
+			}
+
+		}
+		movement.y -= gravity * Time.deltaTime;
+		controller.Move (movement * Time.deltaTime);
+		//if active platform is true then add platform speed to movement
+	
+
+
+	}
+	void OnPlatform () {
+		//set active platform to the platform character is on
+		activePlatform = true;
+		Debug.Log ("works");
+	}
+
+	void OffPlatform () {
+		activePlatform = false;
+		Debug.Log ("alsoworks");
+	}
+
+	void KillPlayer(){
+		Application.LoadLevel(0);
+	}
+		/*
      * TO DO:
      * Movement
      *      Should use the CharacterController which is already attached to this GameObject
@@ -35,5 +82,4 @@ public class PlayerMovement : MonoBehaviour {
      *      A value to keep track of the player's movement speed and direction
      *      You will probably need to use the Update function as well as create functions for moving platforms and enemies
      */
-
-}
+	}
